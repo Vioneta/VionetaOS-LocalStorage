0d9dbc46e10532318424e4ba8e47b5aa4bb3d585
charith <nikhuge@gmail.com>
2024-05-12 03:07:13 +0530

    init

--- drivers/google_drive/meta.go
+++ drivers/google_drive/meta.go
@@ -5,9 +5,11 @@ import (
 	"github.com/IceWhaleTech/CasaOS-LocalStorage/internal/op"
 )
 
-const ICONURL = "./img/driver/GoogleDrive.svg"
-const CLIENTID = "921743327851-urr4f7jjfp4ts639evqb3i4m4qb4u4cc.apps.googleusercontent.com"
-const CLIENTSECRET = "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"
+const (
+	ICONURL      = "./img/driver/GoogleDrive.svg"
+	CLIENTID     = "921743327851-urr4f7jjfp4ts639evqb3i4m4qb4u4cc.apps.googleusercontent.com"
+	CLIENTSECRET = ""
+)
 
 type Addition struct {
 	driver.RootID
@@ -15,7 +17,7 @@ type Addition struct {
 	OrderBy        string `json:"order_by" type:"string" help:"such as: folder,name,modifiedTime" omit:"true"`
 	OrderDirection string `json:"order_direction" type:"select" options:"asc,desc" omit:"true"`
 	ClientID       string `json:"client_id" required:"true" default:"921743327851-urr4f7jjfp4ts639evqb3i4m4qb4u4cc.apps.googleusercontent.com" omit:"true"`
-	ClientSecret   string `json:"client_secret" required:"true" default:"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" omit:"true"`
+	ClientSecret   string `json:"client_secret" required:"true" default:"" omit:"true"`
 	ChunkSize      int64  `json:"chunk_size" type:"number" help:"chunk size while uploading (unit: MB)" omit:"true"`
 	AuthUrl        string `json:"auth_url" type:"string" default:"https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&client_id=921743327851-urr4f7jjfp4ts639evqb3i4m4qb4u4cc.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fcloudoauth.files.casaos.app&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive&access_type=offline&approval_prompt=force&state=${HOST}%2Fv1%2Frecover%2FGoogleDrive&service=lso&o2v=1&flowName=GeneralOAuthFlow"`
 	Icon           string `json:"icon" type:"string" default:"./img/driver/GoogleDrive.svg"`
